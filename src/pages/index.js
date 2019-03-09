347d12aa5cca943039d69ce67e964bc21fcdf77d
John Peden <john.peden@mtsu.edu>
2019-03-09 01:27:27 -0600

    adds new and improved under constrction page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
+import instagram from "../images/instagram.png"
+import SocialIcon from "../components/social-icon"
 
-
-const IndexPage = () => (
+const UnderConstruction = () => (
   <div>
-    <h1>Under Construction</h1>
+    <h1>John Mahlon Peden</h1>
+    <h2>Under Construction</h2>
+    <SocialIcon src={instagram} url="https://www.instagram.com/johnmahlon/"/> 
   </div>
 )
 
-export default IndexPage
+export default UnderConstruction
