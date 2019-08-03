5d78b2ae12528b8f74d4136a552edb1d5a49382f
John Peden <john.peden@mtsu.edu>
2019-08-03 13:15:08 -0500

    no idea what this is

---
+++ src/components/menu.js
@@ -0,0 +1,10 @@
+import React from "react";
+import menuStyle from "./menu.module.css";
+
+const Menu = () => (
+    <div className={menuStyle.menu}>
+        <h3>Home About Resume Photography </h3>
+    </div>
+);
+
+export default Menu;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Github from "../images/github.png";
 import Instagram from "../images/instagram.png";
 import Me from "../images/me.png";
 import ProfileImage from "../components/profile-image";
+import Menu from "../components/menu";
 
 const Home = () => (
   <div>
     <ProfileImage src={Me}/>
     <h1>John Mahlon Peden</h1>
     <h4>Mobile developer for MTSU, hobbyist photographer, and musician.</h4>
+    <Menu />
     <hr/>
     <SocialIcon url="https://github.com/johnmahlon" src={ Github }/>
     <SocialIcon url="https://instagram.com/johnmahlon" src ={ Instagram }/>
